55c2d3da4c86afdb169aa1514acef63b0dac8f5f
7777777_4547 <83630775+7777777-4547@users.noreply.github.com>
2022-07-25 16:05:56 +0800

    test

--- src/main/java/me/jellysquid/mods/sodium/client/gui/options/OptionImpact.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/options/OptionImpact.java
@@ -5,10 +5,10 @@ import net.minecraft.text.TranslatableText;
 
 public enum OptionImpact {
     LOW(Formatting.GREEN, new TranslatableText("sodium.option_impact.low").getString()),
-    MEDIUM(Formatting.YELLOW, new TranslatableText("sodium.option_impact.medium".getString()),
-    HIGH(Formatting.GOLD, new TranslatableText("sodium.option_impact.high".getString()),
-    EXTREME(Formatting.RED, new TranslatableText("sodium.option_impact.extreme".getString()),
-    VARIES(Formatting.WHITE, new TranslatableText("sodium.option_impact.varies".getString());
+    MEDIUM(Formatting.YELLOW, new TranslatableText("sodium.option_impact.medium").getString()),
+    HIGH(Formatting.GOLD, new TranslatableText("sodium.option_impact.high").getString()),
+    EXTREME(Formatting.RED, new TranslatableText("sodium.option_impact.extreme").getString()),
+    VARIES(Formatting.WHITE, new TranslatableText("sodium.option_impact.varies").getString());
 
     private final Formatting color;
     private final String text;
